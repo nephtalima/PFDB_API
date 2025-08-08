15e12e33c33bb08cd77edab5a51290b9143b6865
Aethelhelm <peter.lisulov@outlook.com>
2025-08-08 18:36:54 -0400

    made a build class to abstract away from the main file

---
+++ ComponentTester/Build.cs
@@ -0,0 +1,20 @@
+using PFDB.PythonTesting; //use actual build namespace
+using PFDB.WeaponUtility;
+
+namespace PFDB;
+
+public static class Build
+{
+    //public static IEnumerable<uint> RequiredNumberOfParameters { get; } = new List<uint> { 3, 4 };
+	
+	public static void BuildVersion(PhantomForcesVersion version, string? pythonProgramPath, string? imageBasePath, string? tessbinPath){
+		
+	}
+	
+	public static void BuildAll(string? pythonProgramPath, string? imageBasePath, string? tessbinPath){
+		
+	}
+	
+	
+	
+}
